6819501ed85ac21f0bbb7f88d9cd32df9dfa1c85
Fredrik Domert Eriksson <fredrik.domert@gmail.com>
2024-12-04 16:28:53 +0100

    Uppdatering

---
+++ Laboration3_test/Book.cs
@@ -0,0 +1,45 @@
+namespace ConsoleApp10;
+
+public class Book
+{
+    // Privata fält
+    private string title;
+    private int numOfPages;
+    private string author;
+    private string isbn;
+    
+    
+    
+    // Konstruktor
+    public Book(string title, int numOfPages, string author, string isbn) // <-- default konstruktorn. Vi behöver ändra för att sätta värden
+    {
+        this.title = title;
+        // Fyll på med parametrarna
+        // i klassdiagrammet (UML)
+        this.numOfPages = numOfPages;
+        this.author = author;
+        this.isbn = isbn;
+
+        GenerateISBN();
+        GetBookData();
+        
+
+    } 
+
+    
+    // Metod för att skapa upp ISBN
+    private void GenerateISBN()
+    {
+        Random random = new Random();
+        string isbnTemp = random.Next(100000000, 999999999).ToString();
+        isbnTemp += $"-{random.Next(1, 9).ToString()}";
+        this.isbn = isbnTemp;
+        
+    }
+
+    // Metod för att hämta en sträng med bokdata
+    public string GetBookData()
+    {
+        return $"Title: {title} /n och så fyller ni på med övriga data här";
+    }
+}
---
+++ Laboration3_test/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Book book1 = new("C#", 123, "Fredrik Domert");
+        Book book2 = new("C# 2", 123, "Pär Hedström");
+
+        Book[] myBooks = new Book[2]; // skapar en ny array
+        myBooks[0] = book1; // lägger in Book-objektet i arrayen på olika positioner
+        myBooks[1] = book2;
+        
+        
+        //Loopar igenom arrayen i en for-loop
+        for (int i = 0; i < myBooks.Length; i++)
+        {
+            //för varje varv i loopen skriver vi ut data om bokobjektet vi har
+            // på den i:te positionen
+            Console.WriteLine(myBooks[i].GetBookData());
+        }
+    }
+}
