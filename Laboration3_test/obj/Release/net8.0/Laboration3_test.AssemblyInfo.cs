a5fe5276147d1e077da66b1d50d4aaeda4e0776e
Pär Hedström <h24parhe@du.se>
2024-12-12 10:11:35 +0100

    .

--- Laboration3_test/obj/Release/net8.0/Laboration3_test.AssemblyInfo.cs
+++ Laboration3_test/obj/Release/net8.0/Laboration3_test.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboration3_test")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d243eb5fd71d68ffbfd70b549aae16cb9119f84e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboration3_test")]
